00819fa8dc19fa9ff858d85614d311a69abdce9a
terryblue99 <terryblue@comcast.net>
2018-12-09 16:06:12 -0600

    Updated

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
+const Timestamp = require('react-timestamp')
+
 class Order extends React.Component {
  
   render() {
@@ -10,8 +12,8 @@ class Order extends React.Component {
         <ul>
           <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
           <li>{this.props.size}</li>
-          <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
-          <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
+          <li>{this.props.scoops.length} scoop/s: {this.props.scoops.join(', ')}</li>
+          <li>Ordered by {this.props.orderInfo.customerName} on {this.props.orderInfo.orderedAt}.</li>
         </ul>
       </div>
     );
@@ -24,11 +26,21 @@ Order.defaultProps = {
 }
 
 Order.propTypes = {
-	cone: PropTypes.bool,
-  	size: PropTypes.string,
-  	scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
-  	orderInfo: PropTypes.shape({
+  cone: PropTypes.bool,
+  size: PropTypes.string,
+  scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
+  orderInfo: PropTypes.shape({
     customerName: PropTypes.string.isRequired,
-    orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
-  	}).isRequired
-}
+    orderedAt: PropTypes.isRequired // We're using UNIX timestamps here
+  }).isRequired
+};
+
+ReactDOM.render(
+  <Order 
+  	cone={false}
+  	size='medium'
+  	scoops={['vanilla', 'cherry']}
+  	orderInfo={{customerName: 'Terry Blue', orderedAt: <Timestamp format='full' />}}
+  />,	
+  document.getElementById('root')
+)
